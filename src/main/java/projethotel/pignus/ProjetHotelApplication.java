b4d3289641de03d2900de9e8932da3a58e8e19a0
baabdou <baabdouaziz3@gmail.com>
2021-06-08 11:07:40 +0000

    link the online database

--- src/main/java/projethotel/pignus/ProjetHotelApplication.java
+++ src/main/java/projethotel/pignus/ProjetHotelApplication.java
@@ -1,9 +1,18 @@
 package projethotel.pignus;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import projethotel.pignus.entities.AppRole;
+import projethotel.pignus.entities.AppUser;
+import projethotel.pignus.services.AccountService;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import projethotel.pignus.services.UtilisateurService;
+
 
 @SpringBootApplication
 public class ProjetHotelApplication {
@@ -12,8 +21,25 @@ public class ProjetHotelApplication {
         return new BCryptPasswordEncoder();
     }
 
+    @Autowired
+    private RepositoryRestConfiguration repositoryRestConfiguration;
+
+
     public static void main(String[] args) {
         SpringApplication.run(ProjetHotelApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(AccountService accountService, UtilisateurService utilisateurService) {
+        repositoryRestConfiguration.exposeIdsFor(AppUser.class);
+        return args -> {
+            //accountService.saveRole(new AppRole(null,"USER"));
+            //accountService.saveRole(new AppRole(null,"ADMIN"));
+            //accountService.saveUser("Ousseynou","Gueye","776406502","admin","Bambilor", "M","admin","admin");
+            //accountService.saveUser("Ousseynou","Gueye","776406520","administrator","Bambilor", "M","admin","admin");
+            //utilisateurService.saveUtilisateur("Ousseynou","Gueye","admin","776406502","5", "Bambilor","admin","admin");
+            //accountService.addRoleToUser("administrator","ADMIN");
+            //accountService.addRoleToUser("admin","USER");
+        };
+    }
 }
